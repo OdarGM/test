b66ad43d6f06d76da68052bffbc4e6dda63598da
Odar <radoradev770@gmail.com>
2018-07-04 21:15:37 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function tabSwitch(pageName, elmnt, color){
     var i, contxd, tabs;
     contxd = document.getElementsByClassName("contxd");
-    for(i =0; i< contxd.length; i++){
+    for(i = 0; i< contxd.length; i++){
         contxd[i].style.display = "none";
     }
     tabs = document.getElementsByClassName("tab");
@@ -12,20 +12,5 @@ function tabSwitch(pageName, elmnt, color){
     elmnt.style.backgroundColor = color
 
 }
-document.getElementById("deftab").click();
 
-
-function openPage(pageName,elmnt,color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
-
-}
+//document.getElementById("defshow").click();
